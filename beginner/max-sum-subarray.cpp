94c2361a85d518e5f63ce73de4e067bfff958e67
Matthew Conrad <matthew.conrad@outlook.com>
2021-03-15 19:14:54 -0700

    Add maximum sum subarray problem

---
+++ beginner/max-sum-subarray.cpp
@@ -0,0 +1,13 @@
+/*
+ * Maximum Sum Subarray Problem (Kadane's Algorithm)
+ * 
+ * Given an integer array, find a contiguous array within it with the largest sum.
+ * 
+ * Example:
+ * 
+ * Input:  {-2, 1, -3, 4, -1, 2, 1, -5, 4}
+ * Output: Subarray with the largest sum is {4, -1, 2, 1} with sum 6.
+ */
+
+// Linear O(n) Solution Using Kadane's Algorithm
+
