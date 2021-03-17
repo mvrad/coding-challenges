cddd8a21562a2597141c1d722d341ecda157349d
Matthew Conrad <matthew.conrad@outlook.com>
2021-03-16 19:24:28 -0700

    Add solution to maximum sum subarray problem

--- beginner/max-sum-subarray.cpp
+++ beginner/max-sum-subarray.cpp
@@ -11,3 +11,36 @@
 
 // Linear O(n) Solution Using Kadane's Algorithm
 
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+void maxSubarray(int arr[], int len)
+{
+  int bestSum = INT_MIN, currentSum = 0, 
+    start = 0, s = 0, end = 0;
+  for (int i = 0; i < len; i++)
+  {
+    currentSum += arr[i];
+    if (bestSum < currentSum)
+    {
+      bestSum = currentSum; start = s; end = i;
+    }
+    if (currentSum < 0)
+    {
+      currentSum = 0; s = i + 1;
+    }
+  }
+  cout << "Maximum sum subarray is: " << bestSum << endl;
+  cout << "Starting index: " << start << " Ending index: " << end << endl;
+}
+
+int main()
+{
+  int arr[] = {-2, -3, 4, -1, -2, 1, 5, -3},
+    len = sizeof(arr)/sizeof(arr[0]);
+
+  maxSubarray(arr, len); 
+  return 0;
+}
