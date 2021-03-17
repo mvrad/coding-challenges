b59868ab38c1326ef0d1bf4d434f3773a24f5163
Matthew Conrad <matthew.conrad@outlook.com>
2021-03-17 01:37:15 -0700

    Add binary tree description

--- beginner/left-view-of-a-binary-tree.cpp
+++ beginner/left-view-of-a-binary-tree.cpp
@@ -2,9 +2,19 @@
  * Print Left View of a Binary Tree
  * 
  * Given a binary tree, write an efficient algorithm to print its left view.
+ * 
+ *             1
+ *           /  \
+ *         2     3
+ *       /      / \
+ *     4      5    6
+ *          /       \
+ *        7          8
+ * 
+ * Output: 1, 2, 4, 7
  */
 
-// 1. Iterative Implementation
+// 1. Iterative Implementation (BFS)
 // Complexity | Time: Linear O(n), Space: Requires O(n) extra space
 
 
@@ -14,6 +24,6 @@
 
 
 
-// 3. Recursive Implementation Using Preorder Traversal
+// 3. Recursive Implementation Using Preorder Traversal (DFS)
 // Complexity | Time: Linear O(n), Space: Requires O(1) extra space
 
